14eedbc33e0c1bd8abfc2ab68d5204ef1bb02182
KevKode <iamkevinarnett@gmail.com>
2020-12-08 00:06:40 -0500

    first write of the plugin

--- src/main/java/com/example/equipmentrestrictor/EquipmentRestrictorConfig.java
+++ src/main/java/com/example/equipmentrestrictor/EquipmentRestrictorConfig.java
@@ -3,17 +3,202 @@ package com.example.equipmentrestrictor;
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
+import net.runelite.client.config.ConfigSection;
 
-@ConfigGroup("equipmentrestrictor")
+@ConfigGroup(EquipmentRestrictorPlugin.CONFIG_GROUP)
 public interface EquipmentRestrictorConfig extends Config
 {
 	@ConfigItem(
-		keyName = "greeting",
-		name = "Welcome Greeting",
-		description = "The message to show to the user when they login"
+		keyName = EquipmentRestrictorPlugin.WHITELIST,
+		name = "Whitelist",
+		description = "Items that will always be equipped, overrides blacklist and all other locks",
+		position = 1
 	)
-	default String greeting()
+	default String getWhitelist()
 	{
-		return "Hello";
+		return "Shortbow";
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.BLACKLIST,
+		name = "Blacklist",
+		description = "Items that will never be equipped, overrides all other locks",
+		position = 2
+	)
+	default String getBlacklist()
+	{
+		return "Bronze pickaxe";
+	}
+
+	@ConfigSection(
+		name = "Equipment Slot Locks",
+		description = "Locks an equipment slot, preventing items from being equipped there (✓ = locked)",
+		position = 3,
+		closedByDefault = true
+	)
+	String equipmentSlotLocks = "equipmentSlotLocks";
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.WEAPON_SLOT,
+		name = "Weapon Slot",
+		description = "Locks the weapon slot",
+		position = 1,
+		section = equipmentSlotLocks
+	)
+	default boolean getWeaponSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.SHIELD_SLOT,
+		name = "Shield Slot",
+		description = "Locks the shield slot",
+		position = 2,
+		section = equipmentSlotLocks
+	)
+	default boolean getShieldSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.HEAD_SLOT,
+		name = "Head Slot",
+		description = "Locks the head slot",
+		position = 3,
+		section = equipmentSlotLocks
+	)
+	default boolean getHeadSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.BODY_SLOT,
+		name = "Body Slot",
+		description = "Locks the body slot",
+		position = 4,
+		section = equipmentSlotLocks
+	)
+	default boolean getBodySlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.LEG_SLOT,
+		name = "Leg Slot",
+		description = "Locks the leg slot",
+		position = 5,
+		section = equipmentSlotLocks
+	)
+	default boolean getLegSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.FEET_SLOT,
+		name = "Feet Slot",
+		description = "Locks the feet slot",
+		position = 6,
+		section = equipmentSlotLocks
+	)
+	default boolean getFeetSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.HAND_SLOT,
+		name = "Hand Slot",
+		description = "Locks the hand slot",
+		position = 7,
+		section = equipmentSlotLocks
+	)
+	default boolean getHandSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.NECK_SLOT,
+		name = "Neck Slot",
+		description = "Locks the neck slot",
+		position = 8,
+		section = equipmentSlotLocks
+	)
+	default boolean getNeckSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.RING_SLOT,
+		name = "Ring Slot",
+		description = "Locks the ring slot",
+		position = 9,
+		section = equipmentSlotLocks
+	)
+	default boolean getRingSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.CAPE_SLOT,
+		name = "Cape Slot",
+		description = "Locks the cape slot",
+		position = 10,
+		section = equipmentSlotLocks
+	)
+	default boolean getCapeSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.AMMO_SLOT,
+		name = "Ammo Slot",
+		description = "Locks the ammo slot",
+		position = 11,
+		section = equipmentSlotLocks
+	)
+	default boolean getAmmoSlotLock()
+	{
+		return false;
+	}
+
+	@ConfigSection(
+		name = "Weapon Type Locks",
+		description = "Locks a equipment type, preventing weapons of that type from being equipped (✓ = locked)",
+		position = 4,
+		closedByDefault = true
+	)
+	String weaponTypeSlotLocks = "weaponTypeSlotLocks";
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.ONE_HANDED,
+		name = "One Handed",
+		description = "Locks one handed weapons",
+		position = 1,
+		section = weaponTypeSlotLocks
+	)
+	default boolean getOneHandedLock()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		keyName = EquipmentRestrictorPlugin.TWO_HANDED,
+		name = "Two Handed",
+		description = "Locks two handed weapons",
+		position = 2,
+		section = weaponTypeSlotLocks
+	)
+	default boolean getTwoHandedLock()
+	{
+		return false;
 	}
 }
--- src/main/java/com/example/equipmentrestrictor/EquipmentRestrictorPlugin.java
+++ src/main/java/com/example/equipmentrestrictor/EquipmentRestrictorPlugin.java
@@ -3,14 +3,23 @@ package com.example.equipmentrestrictor;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
+
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.game.ItemManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.util.Text;
+import net.runelite.client.util.WildcardMatcher;
+import net.runelite.api.EquipmentInventorySlot;
 
 @Slf4j
 @PluginDescriptor(
@@ -24,25 +33,155 @@ public class EquipmentRestrictorPlugin extends Plugin
 	@Inject
 	private EquipmentRestrictorConfig config;
 
+	@Inject
+	private ItemManager itemManager;
+
+	static final String CONFIG_GROUP = "equipmentrestrictor";
+	static final String WHITELIST = "whitelist";
+	static final String BLACKLIST = "blacklist";
+	static final String WEAPON_SLOT = "weaponSlot";
+	static final String SHIELD_SLOT = "shieldSlot";
+	static final String HEAD_SLOT = "headSlot";
+	static final String BODY_SLOT = "bodySlot";
+	static final String LEG_SLOT = "legSlot";
+	static final String FEET_SLOT = "feetSlot";
+	static final String HAND_SLOT = "handSlot";
+	static final String NECK_SLOT = "neckSlot";
+	static final String RING_SLOT = "ringSlot";
+	static final String CAPE_SLOT = "capeSlot";
+	static final String AMMO_SLOT = "ammoSlot";
+	static final String ONE_HANDED = "oneHanded";
+	static final String TWO_HANDED = "twoHanded";
+
+	private static final String WIELD = "Wield";
+	private static final String WEAR = "Wear";
+
+	private List<String> whitelistItems;
+	private List<String> blacklistItems;
+	private Hashtable<Integer,Boolean> slotLocks;
+
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked event)
+	{
+		final String menuOption = event.getMenuOption();
+
+		if (menuOption.equals(WIELD) || menuOption.equals(WEAR))
+		{
+			final String itemName = itemManager.getItemComposition(event.getId()).getName();
+			final int itemSlot = itemManager.getItemStats(event.getId(), true).getEquipment().getSlot();
+			final boolean itemIsTwoHanded = itemManager.getItemStats(event.getId(), true).getEquipment().isTwoHanded();
+
+			if (!canPlayerEquipItem(itemName, itemSlot, itemIsTwoHanded))
+			{
+				client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "You can't \"" + menuOption + "\" restricted item \"" + itemName + "\"", null);
+				event.consume();
+			}
+		}
+	}
+
+	private boolean canPlayerEquipItem(String itemName, int itemSlot, boolean itemIsTwoHanded)
+	{
+		// The player can equip the item if...
+		return (
+			itemInList(itemName, whitelistItems)                             // the item is in the whitelist.
+			|| (                                                             // 	(only proceed if the item isn't in the whitelist, it overrides everything)
+				!itemInList(itemName, blacklistItems)                        // the item is not in the black list...
+				&&                                                           // 	and... (proceed to check if the item's slot is locked)
+				!slotLocks.get(itemSlot)                                     // the items slot is not locked...
+				&& (                                                         // 	and... (proceed to check if the item is a weapon, and if it's type is locked)
+					(itemSlot != EquipmentInventorySlot.WEAPON.getSlotIdx()) // the item is not a weapon, it does not need to check handedness.
+					|| (                                                     //		(only proceed if the item is a weapon, armor isn't subject to handedness)
+						itemIsTwoHanded && !config.getTwoHandedLock()        // the item is 2h, and the 2h type is not locked.
+					)
+					|| (                                                     //		(only proceed if the item isn't 2h, need to check 1h lock)
+						!itemIsTwoHanded && !config.getOneHandedLock()       // the item is 1h, and the 1h type is not locked.
+					)
+				)
+			)
+		);
+	}
+
+	private boolean itemInList(String itemName, List<String> itemList)
+	{
+		return itemList.stream().anyMatch(listItemName -> WildcardMatcher.matches(listItemName, itemName));
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		if (event.getGroup().equals(CONFIG_GROUP))
+		{
+			switch (event.getKey()) {
+				case WHITELIST:
+					whitelistItems = Text.fromCSV(config.getWhitelist());
+					break;
+				case BLACKLIST:
+					blacklistItems = Text.fromCSV(config.getBlacklist());
+					break;
+				case HEAD_SLOT:
+					slotLocks.put(EquipmentInventorySlot.HEAD.getSlotIdx(), config.getHeadSlotLock());
+					break;
+				case CAPE_SLOT:
+					slotLocks.put(EquipmentInventorySlot.CAPE.getSlotIdx(), config.getCapeSlotLock());
+					break;
+				case NECK_SLOT:
+					slotLocks.put(EquipmentInventorySlot.AMULET.getSlotIdx(), config.getNeckSlotLock());
+					break;
+				case WEAPON_SLOT:
+					slotLocks.put(EquipmentInventorySlot.WEAPON.getSlotIdx(), config.getWeaponSlotLock());
+					break;
+				case BODY_SLOT:
+					slotLocks.put(EquipmentInventorySlot.BODY.getSlotIdx(), config.getBodySlotLock());
+					break;
+				case SHIELD_SLOT:
+					slotLocks.put(EquipmentInventorySlot.SHIELD.getSlotIdx(), config.getShieldSlotLock());
+					break;
+				case LEG_SLOT:
+					slotLocks.put(EquipmentInventorySlot.LEGS.getSlotIdx(), config.getLegSlotLock());
+					break;
+				case HAND_SLOT:
+					slotLocks.put(EquipmentInventorySlot.GLOVES.getSlotIdx(), config.getHandSlotLock());
+					break;
+				case FEET_SLOT:
+					slotLocks.put(EquipmentInventorySlot.BOOTS.getSlotIdx(), config.getFeetSlotLock());
+					break;
+				case RING_SLOT:
+					slotLocks.put(EquipmentInventorySlot.RING.getSlotIdx(), config.getRingSlotLock());
+					break;
+				case AMMO_SLOT:
+					slotLocks.put(EquipmentInventorySlot.AMMO.getSlotIdx(), config.getAmmoSlotLock());
+					break;
+
+			}
+		}
+	}
+
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Equipment Restrictor started!");
+		whitelistItems = Text.fromCSV(config.getWhitelist());
+		blacklistItems = Text.fromCSV(config.getBlacklist());
+
+		slotLocks = new Hashtable<>();
+		slotLocks.put(EquipmentInventorySlot.HEAD.getSlotIdx(), config.getHeadSlotLock());
+		slotLocks.put(EquipmentInventorySlot.CAPE.getSlotIdx(), config.getCapeSlotLock());
+		slotLocks.put(EquipmentInventorySlot.AMULET.getSlotIdx(), config.getNeckSlotLock());
+		slotLocks.put(EquipmentInventorySlot.WEAPON.getSlotIdx(), config.getWeaponSlotLock());
+		slotLocks.put(EquipmentInventorySlot.BODY.getSlotIdx(), config.getBodySlotLock());
+		slotLocks.put(EquipmentInventorySlot.SHIELD.getSlotIdx(), config.getShieldSlotLock());
+		slotLocks.put(EquipmentInventorySlot.LEGS.getSlotIdx(), config.getLegSlotLock());
+		slotLocks.put(EquipmentInventorySlot.GLOVES.getSlotIdx(), config.getHandSlotLock());
+		slotLocks.put(EquipmentInventorySlot.BOOTS.getSlotIdx(), config.getFeetSlotLock());
+		slotLocks.put(EquipmentInventorySlot.RING.getSlotIdx(), config.getRingSlotLock());
+		slotLocks.put(EquipmentInventorySlot.AMMO.getSlotIdx(), config.getAmmoSlotLock());
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Equipment Restrictor stopped!");
-	}
-
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Equipment Restrictor says " + config.greeting(), null);
-		}
+		whitelistItems.clear();
+		blacklistItems.clear();
+		slotLocks.clear();
 	}
 
 	@Provides
